52bbcef39812fe8da05d53d6b9ab5e85fd3e3bf3
Madhav Parmar <152685058+madhavparmar11@users.noreply.github.com>
2025-03-06 11:16:33 +0530

    Add files via upload

---
+++ Spring_Intro2-UC2/SpringNew/src/main/java/com/SpringNew/SpringNew/GreetingController.java
@@ -0,0 +1,24 @@
+package com.SpringNew.SpringNew;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+
+    private final GreetingService greetingService;
+
+    @Autowired
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    @GetMapping("/message")
+    public ResponseEntity<String> getGreetingMessage() {
+        return ResponseEntity.ok(greetingService.getGreeting());
+    }
+}
---
+++ Spring_Intro2-UC2/SpringNew/src/main/java/com/SpringNew/SpringNew/GreetingService.java
@@ -0,0 +1,10 @@
+package com.SpringNew.SpringNew;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+    public String getGreeting() {
+        return "Hello World";
+    }
+}
